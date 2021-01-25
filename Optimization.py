ef245198cfb4938a7e2275b25c6d212d075424d4
Shehab123 <67390061+shehababaaas@users.noreply.github.com>
2021-01-25 15:16:58 +0200

    Update Optimization.py

--- Optimization.py
+++ Optimization.py
@@ -7,22 +7,30 @@ import matplotlib.pyplot as plt
 s=[]
 s1=[]
 s2=[]
-def funv(x):
+def objective(x):
     x1=x[0]
     y=x[1]
-    s.append(x1)
-    s1.append(y)
-    funcv=((x1**2) - (10*x1) + (y**2) - (14*y) + 28)  
+    funcv=((x1**2) - (10*x1) + (y**2) - (14*y) + 28) 
     s2.append(funcv)
     return (funcv)  
-def constraint(x):
+def constraint_1(x):
   x1=x[0]
   y=x[1]
-  return (x1+y-8)
+  con_1=(x1+y-8)
+  s1.append(con_1)
+  return (con_1)
+def constraint_2(x,con_2):
+  x1=x[0]
+  y=x[1]
+  con_2=np.nan
+  s2.append(con_2)
+  return (con_2)  
 x_int=float(input('x_intialize:'))
 y_int=float(input('y_intialize:'))
 x0=np.array([x_int,y_int])
-con1=({'type':'eq','fun':constraint})
+con1=({'type':'eq','fun':constraint_1})
+con2=({'type':'ineq','fun':constraint_2})
+cons=[con1,con2]
 sol=minimize(objective,x0,method='SLSQP',constraints=con1,options = {"disp": True})
 xop=sol.x
 vop=sol.fun
